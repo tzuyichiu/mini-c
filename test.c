c5ec0909aba304dcf621489bab0dd9a0d1fff202
Tzu-yi CHIU <tzu-yi.chiu@polytechnique.edu>
2020-02-03 16:31:13 +0100

    Fixed all typing problems

--- test.c
+++ test.c
@@ -1,16 +1,1 @@
-// un exemple de fichier mini-C
-// à modifier au fur et à mesure des tests
-//
-// la commande 'make' recompile mini-c (si nécessaire)
-// et le lance sur ce fichier
-
-int hello(int x) {
-    return x;
-}
-
-int main() {
-    int x;
-    int x;
-    x = -3;
-    return x;
-}
+int main() { { int x; } x; }
